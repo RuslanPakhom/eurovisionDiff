ab57c9ee9ac0064912080a283769d46ed77c2066
Ruslan Pakhomov <55085288+RuslanPakhom@users.noreply.github.com>
2024-03-15 18:12:09 +0100

    Project

---
+++ script.js
@@ -0,0 +1,487 @@
+const participaints2023 = [
+    
+        {
+            "country": "Albania",
+            "artist": "Albina and Familja Kelmendi",
+            "song": "Duje"
+        },
+        {
+            "country": "Armenia",
+            "artist": "Brunette",
+            "song": "Future Lover"
+        },
+        {
+            "country": "Australia",
+            "artist": "Voyager",
+            "song": "Promise"
+        },
+        {
+            "country": "Austria",
+            "artist": "Teya and Salena",
+            "song": "Who the Hell Is Edgar?"
+        },
+        {
+            "country": "Azerbaijan",
+            "artist": "TuralTuranX",
+            "song": "Tell Me More"
+        },
+        {
+            "country": "Belgium",
+            "artist": "Gustaph",
+            "song": "Because of You"
+        },
+        {
+            "country": "Croatia",
+            "artist": "Let 3",
+            "song": "Mama ŠČ!"
+        },
+        {
+            "country": "Cyprus",
+            "artist": "Andrew Lambrou",
+            "song": "Break a Broken Heart"
+        },
+        {
+            "country": "Czechia",
+            "artist": "Vesna",
+            "song": "My Sister's Crown"
+        },
+        {
+            "country": "Denmark",
+            "artist": "Reiley",
+            "song": "Breaking My Heart"
+        },
+        {
+            "country": "Estonia",
+            "artist": "Alika",
+            "song": "Bridges"
+        },
+        {
+            "country": "Finland",
+            "artist": "Käärijä",
+            "song": "Cha Cha Cha"
+        },
+        {
+            "country": "France",
+            "artist": "La Zarra",
+            "song": "Évidemment"
+        },
+        {
+            "country": "Georgia",
+            "artist": "Iru",
+            "song": "Echo"
+        },
+        {
+            "country": "Germany",
+            "artist": "Lord of the Lost",
+            "song": "Blood & Glitter"
+        },
+        {
+            "country": "Greece",
+            "artist": "Victor Vernicos",
+            "song": "What They Say"
+        },
+        {
+            "country": "Iceland",
+            "artist": "Diljá",
+            "song": "Power"
+        },
+        {
+            "country": "Ireland",
+            "artist": "Wild Youth",
+            "song": "We Are One"
+        },
+        {
+            "country": "Israel",
+            "artist": "Noa Kirel",
+            "song": "Unicorn"
+        },
+        {
+            "country": "Italy",
+            "artist": "Marco Mengoni",
+            "song": "Due vite"
+        },
+        {
+            "country": "Latvia",
+            "artist": "Sudden Lights",
+            "song": "Aijā"
+        },
+        {
+            "country": "Lithuania",
+            "artist": "Monika Linkytė",
+            "song": "Stay"
+        },
+        {
+            "country": "Malta",
+            "artist": "The Busker",
+            "song": "Dance (Our Own Party)"
+        },
+        {
+            "country": "Moldova",
+            "artist": "Pasha Parfeni",
+            "song": "Soarele și luna"
+        },
+        {
+            "country": "Netherlands",
+            "artist": "Mia Nicolai and Dion Cooper",
+            "song": "Burning Daylight"
+        },
+        {
+            "country": "Norway",
+            "artist": "Alessandra",
+            "song": "Queen of Kings"
+        },
+        {
+            "country": "Poland",
+            "artist": "Blanka",
+            "song": "Solo"
+        },
+        {
+            "country": "Portugal",
+            "artist": "Mimicat",
+            "song": "Ai coração"
+        },
+        {
+            "country": "Romania",
+            "artist": "Theodor Andrei",
+            "song": "D.G.T. (Off and On)"
+        },
+        {
+            "country": "San Marino",
+            "artist": "Piqued Jacks",
+            "song": "Like an Animal"
+        },
+        {
+            "country": "Serbia",
+            "artist": "Luke Black",
+            "song": "Samo mi se spava"
+        },
+        {
+            "country": "Slovenia",
+            "artist": "Joker Out",
+            "song": "Carpe Diem"
+        },
+        {
+            "country": "Spain",
+            "artist": "Blanca Paloma",
+            "song": "Eaea"
+        },
+        {
+            "country": "Sweden",
+            "artist": "Loreen",
+            "song": "Tattoo"
+        },
+        {
+            "country": "Switzerland",
+            "artist": "Remo Forrer",
+            "song": "Watergun"
+        },
+        {
+            "country": "Ukraine",
+            "artist": "Tvorchi",
+            "song": "Heart of Steel"
+        },
+        {
+            "country": "United Kingdom",
+            "artist": "Mae Muller",
+            "song": "I Wrote a Song"
+        }
+    ]
+
+
+const paricipaints2024 = [
+    {
+        "country": "Albania",
+        "artist": "Besa",
+        "song": "Titan"
+    },
+    {
+        "country": "Armenia",
+        "artist": "Ladaniva",
+        "song": "Jako (Ժակո)"
+    },
+    {
+        "country": "Australia",
+        "artist": "Electric Fields",
+        "song": "One Milkali (One Blood)"
+    },
+    {
+        "country": "Austria",
+        "artist": "Kaleen",
+        "song": "We Will Rave"
+    },
+    {
+        "country": "Azerbaijan",
+        "artist": "Fahree feat. Ilkin Dovlatov",
+        "song": "Özünlə apar"
+    },
+    {
+        "country": "Belgium",
+        "artist": "Mustii",
+        "song": "Before the Party's Over"
+    },
+    {
+        "country": "Croatia",
+        "artist": "Baby Lasagna",
+        "song": "Rim Tim Tagi Dim"
+    },
+    {
+        "country": "Cyprus",
+        "artist": "Silia Kapsis",
+        "song": "Liar"
+    },
+    {
+        "country": "Czechia",
+        "artist": "Aiko",
+        "song": "Pedestal"
+    },
+    {
+        "country": "Denmark",
+        "artist": "Saba",
+        "song": "Sand"
+    },
+    {
+        "country": "Estonia",
+        "artist": "5miinust and Puuluup",
+        "song": "(Nendest) narkootikumidest ei tea me (küll) midagi"
+    },
+    {
+        "country": "Finland",
+        "artist": "Windows95man",
+        "song": "No Rules!"
+    },
+    {
+        "country": "France",
+        "artist": "Slimane",
+        "song": "Mon amour"
+    },
+    {
+        "country": "Georgia",
+        "artist": "Nutsa Buzaladze",
+        "song": "Firefighter"
+    },
+    {
+        "country": "Germany",
+        "artist": "Isaak",
+        "song": "Always on the Run"
+    },
+    {
+        "country": "Greece",
+        "artist": "Marina Satti",
+        "song": "Zari (Ζάρι)"
+    },
+    {
+        "country": "Iceland",
+        "artist": "Hera Björk",
+        "song": "Scared of Heights"
+    },
+    {
+        "country": "Ireland",
+        "artist": "Bambie Thug",
+        "song": "Doomsday Blue"
+    },
+    {
+        "country": "Israel",
+        "artist": "Eden Golan",
+        "song": "Hurricane"
+    },
+    {
+        "country": "Italy",
+        "artist": "Angelina Mango",
+        "song": "La noia"
+    },
+    {
+        "country": "Latvia",
+        "artist": "Dons",
+        "song": "Hollow"
+    },
+    {
+        "country": "Lithuania",
+        "artist": "Silvester Belt",
+        "song": "Luktelk"
+    },
+    {
+        "country": "Luxembourg",
+        "artist": "Tali",
+        "song": "Fighter"
+    },
+    {
+        "country": "Malta",
+        "artist": "Sarah Bonnici",
+        "song": "Loop"
+    },
+    {
+        "country": "Moldova",
+        "artist": "Natalia Barbu",
+        "song": "In the Middle"
+    },
+    {
+        "country": "Netherlands",
+        "artist": "Joost Klein",
+        "song": "Europapa"
+    },
+    {
+        "country": "Norway",
+        "artist": "Gåte",
+        "song": "Ulveham"
+    },
+    {
+        "country": "Poland",
+        "artist": "Luna",
+        "song": "The Tower"
+    },
+    {
+        "country": "Portugal",
+        "artist": "Iolanda",
+        "song": "Grito"
+    },
+    {
+        "country": "San Marino",
+        "artist": "Megara",
+        "song": "11:11"
+    },
+    {
+        "country": "Serbia",
+        "artist": "Teya Dora",
+        "song": "Ramonda"
+    },
+    {
+        "country": "Slovenia",
+        "artist": "Raiven",
+        "song": "Veronika"
+    },
+    {
+        "country": "Spain",
+        "artist": "Nebulossa",
+        "song": "Zorra"
+    },
+    {
+        "country": "Sweden",
+        "artist": "Marcus & Martinus",
+        "song": "Unforgettable"
+    },
+    {
+        "country": "Switzerland",
+        "artist": "Nemo",
+        "song": "The Code"
+    },
+    {
+        "country": "Ukraine",
+        "artist": "Alyona Alyona and Jerry Heil",
+        "song": "Teresa & Maria"
+    },
+    {
+        "country": "United Kingdom",
+        "artist": "Olly Alexander",
+        "song": "Dizzy"
+    },
+]
+
+liked2023 = []
+liked2024 = []
+
+let euro2023 = 0;
+let euro2024 = 0;
+let iteration = 0;
+
+const score2023 = document.querySelector('#score-2023')
+const score2024 = document.querySelector('#score-2024')
+const roundContainer = document.querySelector('.round img')
+
+const btn2023 = document.querySelector('#btn2023')
+const btn2024 = document.querySelector('#btn2024')
+
+const pcountry2023 = document.querySelector('#btn2023 .country')
+const partist2023 = document.querySelector('#btn2023 .artist')
+const psong2023 = document.querySelector('#btn2023 .song')
+
+const pcountry2024 = document.querySelector('#btn2024 .country')
+const partist2024 = document.querySelector('#btn2024 .artist')
+const psong2024 = document.querySelector('#btn2024 .song')
+
+const round = document.querySelector('.round');
+
+
+
+btn2023.addEventListener('click', () => {
+    euro2023++; 
+    let country = showCountry(2023);
+});
+
+btn2024.addEventListener('click', () => {
+    euro2024++; 
+    let country = showCountry(2024) 
+
+});
+
+
+
+
+function showCountry(year){
+    score2023.textContent = euro2023;
+    score2024.textContent = euro2024;
+    let country = participaints2023[iteration]['country'];
+    if(country === "Romania") {
+        iteration++;
+        country = participaints2023[iteration]['country']
+    }
+    const obj2023 = participaints2023[iteration];
+    const obj2024 = paricipaints2024.filter(obj => {
+        return obj.country == country
+    })[0];
+
+    roundContainer.src = `assets/${country.toLowerCase()}.png`;
+
+    pcountry2023.textContent = `${obj2023.country}-2023`
+    partist2023.textContent = obj2023.artist;
+    psong2023.textContent = obj2023.song;
+
+    pcountry2024.textContent = `${obj2024.country}-2024`
+    partist2024.textContent = obj2024.artist;
+    psong2024.textContent = obj2024.song;
+
+    if(year == 2023){
+        liked2023.push(country)
+    }
+
+    if(year == 2024){
+        liked2024.push(country)
+    }
+ 
+    iteration++;
+    if(iteration >= participaints2023.length){
+        btn2023.disabled = true;
+        btn2024.disabled = true;
+        round.removeChild(roundContainer);
+
+        console.log(liked2023);
+        console.log(liked2024);
+
+        const div2023 = document.createElement('div')
+        div2023.classList.add('liked');
+        round.appendChild(div2023)
+
+        const div2024 = document.createElement('div')
+        div2024.classList.add('liked');
+        round.appendChild(div2024)
+
+
+        liked2023.forEach((val,idx) => {
+            div2023.textContent += val
+            if(idx != liked2023.length-1){
+                div2023.textContent += ", "
+            }
+        });
+
+        liked2024.forEach((val,idx) => {
+            div2024.textContent += val;
+            if(idx != liked2024.length-1){
+                div2024.textContent += ", "
+            }
+        })
+ 
+    }
+
+
+}
+
+showCountry();
